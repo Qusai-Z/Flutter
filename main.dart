7373fe72957194f533714181cd011fff57f27f88
قصي <98464635+Qusai-Z@users.noreply.github.com>
2022-08-18 14:02:46 +0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:rs/BMI.dart';
+import 'package:rs/Counter.dart';
+import 'package:rs/MessagePage.dart';
+import 'package:rs/UsersScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,118 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 44, 44, 44),
-        appBar: AppBar(
-          leading: Icon(Icons.menu_open),
-          backgroundColor: Colors.orange,
-          title: Text(
-            'Food Station',
-            style: TextStyle(fontSize: 35),
-          ),
-          actions: [
-            Icon(Icons.access_alarm),
-          ],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: SingleChildScrollView(
-            child: Container(
-              child: Column(
-                children: [
-                  Text(
-                    'Login',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                        hintText: 'Qusai@gmail.com',
-                        labelText: 'Email Address',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                        prefixIcon: Icon(
-                          Icons.email,
-                        ),
-                      ),
-
-                      //textAlign: TextAlign.center,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        hintText:
-                            'Your passwrd should be at least 6 characters',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                        prefixIcon: Icon(
-                          Icons.lock,
-                        ),
-                        suffixIcon: Icon(
-                          (Icons.remove_red_eye),
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 255, 128, 24),
-                      borderRadius: BorderRadius.circular(70),
-                    ),
-                    width: double.infinity,
-                    child: MaterialButton(
-                      onPressed: () {},
-                      child: Text(
-                        'LOGIN',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 15),
-                  Text(
-                    'Don\'t have an account ?',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Rigester now',
-                      style: TextStyle(color: Colors.orange),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
+      home: BMI(),
     );
   }
 }
